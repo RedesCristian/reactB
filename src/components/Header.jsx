660d8f605ada7f104913f7b9f3021538ba98992f
RedesCristian <149571424+RedesCristian@users.noreply.github.com>
2024-07-03 14:48:11 +0300

    modified header structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
-import React from "react";
-import styles from "./Header.module.css";
-
+import React from 'react';
+import styles from './Header.module.css';
+import logo from '../images/logo (1).png';
 const Header = () => {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>Barbershop</h1>
+      <div className={styles.logoImage}>
+        <img src={logo} alt="Company logo" width="50px" height="65px"></img>
+        <h1 className={styles.title}>Barbershop</h1>
+      </div>
       <nav className={styles.nav}>
         <a href="#services">Services</a>
         <a href="#about">About Us</a>
