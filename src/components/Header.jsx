5c551e7cc54998774b05eb49cf7bd4bc1b7519ba
SecaraAdrian <secaforce@gmail.com>
2024-07-04 11:06:21 +0300

    update header and cards

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,13 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logoImage}>
-        <img src={logo} alt="Company logo" width="50px" height="65px"></img>
-        <h1 className={styles.title}>Barbershop</h1>
+        <img src={logo} alt="Company logo" width="135px" height="auto"></img>
+        {/* <h1 className={styles.title}>Barbershop</h1> */}
       </div>
       <nav className={styles.nav}>
-        <a href="#services">Services</a>
-        <a href="#about">About Us</a>
-        <a href="#team">Our Team</a>
+        <a href="#services">Servicii</a>
+        <a href="#about">Poveste</a>
+        <a href="#team">Echipa</a>
         <a href="#gallery">Gallery</a>
         <a href="#contact">Contact</a>
       </nav>
--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import styles from "./Team.module.css";
+import character1 from "../images/team/character1.png";
+import character2 from "../images/team/character2.png";
+
 
 const teamData = [
   {
     name: "John Doe",
     position: "Founder & Head Barber",
-    imgSrc: "path_to_john_image.jpg",
+    imgSrc: character1,
     bio: "John has over 20 years of experience and founded the barbershop.",
   },
   {
     name: "Jane Smith",
     position: "Senior Barber",
-    imgSrc: "path_to_jane_image.jpg",
+    imgSrc: character2,
     bio: "Jane is a highly skilled barber with a passion for styling.",
   },
   // Adaugă mai mulți membri ai echipei după cum este necesar
